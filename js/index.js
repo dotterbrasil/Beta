8ec63ce36b6c818e1c645ad6de303662f94ab5c1
dotterbrasil <eduardo@dotter.com.br>
2014-05-15 16:12:12 -0300

    Biblioteca js

--- js/index.js
+++ js/index.js
@@ -56,7 +56,6 @@ var app = {
         
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-	
         scanner.scan( function (result) { 
 
             alert("We got a barcode\n" + 
